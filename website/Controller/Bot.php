f328d6f5bfd74d052ff8bdfc774d4c91ba1bbe5f
Esteban De La Fuente Rubio <esteban@delaf.cl>
2016-06-04 00:25:15 -0400

    Se mejora el Bot de LibreDTE para Telegram

--- website/Controller/Bot.php
+++ website/Controller/Bot.php
@@ -27,31 +27,33 @@ namespace website;
  * Controlador para LibreDTEbot de Telegram, provee los comandos que el Bot
  * puede ejecutar
  * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
- * @version 2015-10-12
+ * @version 2016-02-07
  */
 class Controller_Bot extends \sowerphp\app\Controller_Bot
 {
 
     protected $help = [
         'timbre'          => 'verificar timbre electrónico',
+        'estadisticas'    => 'consultar estadísticas de LibreDTE',
+        'contribuyente'   => 'obtener los datos básicos de un contribuyente',
     ]; ///< Ayuda del Bot
 
     /**
      * Comando que inicia el Bot
      * @param token No usado, sólo por compatibilidad con método clase padre
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
-     * @version 2015-10-13
+     * @version 2016-06-04
      */
     protected function _bot_start($token = null)
     {
-        $this->Bot->send(__('¡Hola! Soy LibreDTE y conmigo puedes verificar el timbre electrónico (TED) de un documento tributario electrónico (DTE). Sólo debes enviarme la imagen del TED.'));
+        $this->Bot->send(__('¡Hola! Soy LibreDTE, ¿cómo? ¿no me conoces? ¡Regístrate gratis en https://libredte.cl y libérate!'));
     }
 
     /**
      * Comando que verifica el timbre del documento (sus datos)
      * @param ted String con el timbre electrónico
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
-     * @version 2015-10-13
+     * @version 2016-02-08
      */
     protected function _bot_timbre($timbre_xml = null)
     {
@@ -66,19 +68,19 @@ class Controller_Bot extends \sowerphp\app\Controller_Bot
             $this->Bot->sendChatAction();
             $timbre_xml = implode(' ', func_get_args());
             $rest = new \sowerphp\core\Network_Http_Rest();
-            $rest->setAuth(\sowerphp\core\Configure::read('api.default.token'));
+            $rest->setAuth(\sowerphp\core\Configure::read('api.default.token_bot'));
             $response = $rest->post(
                 $this->request->url.'/api/dte/documentos/verificar_ted',
                 json_encode(base64_encode($timbre_xml))
             );
             if ($response['status']['code']!=200) {
-                $this->Bot->send($response['body']);
+                'Lo siento: '.$this->Bot->send($response['body']);
                 return;
             }
             $xml =  new \SimpleXMLElement(utf8_encode($timbre_xml), LIBXML_COMPACT);
             list($rut, $dv) = explode('-', $xml->xpath('/TED/DD/RE')[0]);
             $this->Bot->send(
-                (new \website\Dte\Admin\Model_DteTipo($xml->xpath('/TED/DD/TD')[0]))->tipo.
+                (new \website\Dte\Admin\Mantenedores\Model_DteTipo($xml->xpath('/TED/DD/TD')[0]))->tipo.
                 ' N° '.$xml->xpath('/TED/DD/F')[0].
                 ' del '.\sowerphp\general\Utility_Date::format($xml->xpath('/TED/DD/FE')[0]).
                 ' por $'.num($xml->xpath('/TED/DD/MNT')[0]).'.-'.
@@ -93,7 +95,7 @@ class Controller_Bot extends \sowerphp\app\Controller_Bot
     /**
      * Comando que procesa el envío del timbre como fotografía
      * @param photo ID de la fotografía que envió el usuario
-     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]delaf.cl)
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
      * @version 2015-10-13
      */
     protected function _bot_photo($file_id)
@@ -117,6 +119,65 @@ class Controller_Bot extends \sowerphp\app\Controller_Bot
             return;
         }
         $this->_bot_timbre(base64_decode($response['body']));
+    }
+
+    /**
+     * Comando que muestra las estadísticas de los usuarios y contribuyentes
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+     * @version 2016-03-02
+     */
+    protected function _bot_estadisticas($certificacion = null, $desde = 1, $hasta = 0)
+    {
+        if ($certificacion === null) {
+            $this->setNextCommand('estadisticas');
+            $this->Bot->sendKeyboard('¿de qué ambiente?', [['0 - Producción', '1 - Certificación']]);
+        } else {
+            $this->setNextCommand();
+            $response = (new \sowerphp\core\Network_Http_Rest())->get(
+                $this->request->url.'/api/estadisticas/'.((int)$certificacion?'certificacion':'produccion')
+            );
+            extract($response['body']);
+            $this->Bot->send(
+                sprintf(
+                    'Existen %s proveedores y/o clientes. Hay %s usuarios registrados, estos usuarios han inscrito a %s empresas y emitido un total de %s documentos.',
+                    num($contribuyentes_sii),
+                    num($usuarios_registrados),
+                    num($empresas_registradas),
+                    num($documentos_emitidos)
+                )
+            );
+            if ($contribuyentes_activos) {
+                $razones_sociales = [];
+                foreach ($contribuyentes_activos as $c) {
+                    $razones_sociales[] = $c['razon_social'];
+                }
+                $this->Bot->send('Los contribuyentes con movimientos son:'."\n- ".implode("\n- ", $razones_sociales));
+            }
+            $msg = 'Usuarios mensuales que iniciaron sesión por última vez:'."\n";
+            $usuarios_mostrar = array_slice(array_reverse($usuarios_mensuales), 0, 3);
+            foreach ($usuarios_mostrar as $u) {
+                $msg .= '- '.$u['mes'].' => '.$u['usuarios']."\n";
+            }
+            $this->Bot->send($msg);
+        }
+    }
+
+    /**
+     * Comando que entrega los datos de un contribuyente
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+     * @version 2016-02-07
+     */
+    protected function _bot_contribuyente($rut = null)
+    {
+        if (!$rut) {
+            $this->setNextCommand('contribuyente');
+            $this->Bot->send('¿Qué RUT te interesa consultar?');
+        } else {
+            $this->setNextCommand();
+            $this->Bot->send(json_encode((new \sowerphp\core\Network_Http_Rest())->get(
+                $this->request->url.'/api/dte/contribuyentes/info/'.$rut
+            )['body'], JSON_PRETTY_PRINT));
+        }
     }
 
 }
