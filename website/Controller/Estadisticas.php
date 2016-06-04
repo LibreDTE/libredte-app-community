f846644e7ee40e7030740b201b5bd93aea82d60e
Esteban De La Fuente Rubio <esteban@delaf.cl>
2016-06-04 00:16:39 -0400

    Actualización estadísticas públicas

--- website/Controller/Estadisticas.php
+++ website/Controller/Estadisticas.php
@@ -27,7 +27,7 @@ namespace website;
 /**
  * Controlador para mostrar estadísticas públicas del sitio
  * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
- * @version 2016-01-07
+ * @version 2016-02-07
  */
 class Controller_Estadisticas extends \Controller_App
 {
@@ -35,11 +35,11 @@ class Controller_Estadisticas extends \Controller_App
     /**
      * Método para permitir acciones sin estar autenticado
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
-     * @version 2016-01-07
+     * @version 2016-02-18
      */
     public function beforeFilter()
     {
-        $this->Auth->allow('index', 'produccion', 'certificacion', 'grafico_usuarios_ingreso');
+        $this->Auth->allow('index', 'produccion', 'certificacion');
         parent::beforeFilter();
     }
 
@@ -53,30 +53,10 @@ class Controller_Estadisticas extends \Controller_App
      */
     public function index($certificacion = false, $desde = 1, $hasta = 0)
     {
-        $Contribuyentes = new \website\Dte\Model_Contribuyentes();
-        $contribuyentes_sii = $Contribuyentes->count();
-        $empresas_registradas = $Contribuyentes->countRegistrados($certificacion);
-        $DteEmitidos = new \website\Dte\Model_DteEmitidos();
-        $DteEmitidos->setWhereStatement(
-            ['certificacion = :certificacion'],
-            [':certificacion' => (int)$certificacion]
+        $response = (new \sowerphp\core\Network_Http_Rest())->get(
+            $this->request->url.'/api/estadisticas/'.($certificacion?'certificacion':'produccion')
         );
-        $Usuarios = new \sowerphp\app\Sistema\Usuarios\Model_Usuarios();
-        $Usuarios->setWhereStatement(['activo = true']);
-        try {
-            $contribuyentes_activos = $Contribuyentes->getConMovimientos($desde, $hasta, $certificacion, false);
-        } catch (\sowerphp\core\Exception_Model_Datasource_Database $e) {
-            \sowerphp\core\Model_Datasource_Session::message($e->getMessage(), 'error');
-            $contribuyentes_activos = [];
-        }
-        $this->set([
-            'certificacion' => $certificacion,
-            'contribuyentes_sii' => $contribuyentes_sii,
-            'usuarios_registrados' => $Usuarios->count(),
-            'empresas_registradas' => $empresas_registradas,
-            'documentos_emitidos' => $DteEmitidos->count(),
-            'contribuyentes_activos' => $contribuyentes_activos,
-        ]);
+        $this->set($response['body']);
         $this->autoRender = false;
         $this->render('Estadisticas/index');
     }
@@ -108,17 +88,66 @@ class Controller_Estadisticas extends \Controller_App
     }
 
     /**
-     * Gráfico con ingreso de usuarios mensualmente
+     * API que muestra la página principal de estadísticas para ambiente de
+     * producción
+     * @param desde Desde cuando considerar la actividad de los contribuyentes
+     * @param hasta Hasta cuando considerar la actividad de los contribuyentes
      * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
-     * @version 2016-01-04
+     * @version 2016-02-07
      */
-    public function grafico_usuarios_ingreso()
+    public function _api_produccion_GET($desde = 1, $hasta = 0)
     {
-        $chart = new \sowerphp\general\View_Helper_Chart();
-        $chart->vertical_bar(
-            'Usuarios mensuales que iniciaron sesión por última vez',
-            ['Usuarios mensuales'=>(new \sowerphp\app\Sistema\Usuarios\Model_Usuarios())->getStatsLogin()]
+        $this->getEstadistica(false, $desde, $hasta);
+    }
+
+    /**
+     * API que muestra la página principal de estadísticas para ambiente de
+     * certificación
+     * @param desde Desde cuando considerar la actividad de los contribuyentes
+     * @param hasta Hasta cuando considerar la actividad de los contribuyentes
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+     * @version 2016-02-07
+     */
+    public function _api_certificacion_GET($desde = 1, $hasta = 0)
+    {
+        $this->getEstadistica(true, $desde, $hasta);
+    }
+
+    /**
+     * Método que genera la estadística para las API de producción y
+     * certificación
+     * @param certificacion =true se generan estadísticas para el ambiente de certificación
+     * @param desde Desde cuando considerar la actividad de los contribuyentes
+     * @param hasta Hasta cuando considerar la actividad de los contribuyentes
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+     * @version 2016-02-07
+     */
+    private function getEstadistica($certificacion, $desde, $hasta)
+    {
+        $Contribuyentes = new \website\Dte\Model_Contribuyentes();
+        $contribuyentes_sii = $Contribuyentes->count();
+        $empresas_registradas = $Contribuyentes->countRegistrados($certificacion);
+        $DteEmitidos = new \website\Dte\Model_DteEmitidos();
+        $DteEmitidos->setWhereStatement(
+            ['certificacion = :certificacion'],
+            [':certificacion' => (int)$certificacion]
         );
+        $Usuarios = new \sowerphp\app\Sistema\Usuarios\Model_Usuarios();
+        $Usuarios->setWhereStatement(['activo = true']);
+        try {
+            $contribuyentes_activos = $Contribuyentes->getConMovimientos($desde, $hasta, $certificacion, false);
+        } catch (\sowerphp\core\Exception_Model_Datasource_Database $e) {
+            $this->Api->send($e->getMessage(), 500);
+        }
+        $this->Api->send([
+            'certificacion' => (int)$certificacion,
+            'contribuyentes_sii' => $contribuyentes_sii,
+            'usuarios_registrados' => $Usuarios->count(),
+            'empresas_registradas' => $empresas_registradas,
+            'documentos_emitidos' => $DteEmitidos->count(),
+            'usuarios_mensuales' => (new \sowerphp\app\Sistema\Usuarios\Model_Usuarios())->getStatsLogin(),
+            'contribuyentes_activos' => $contribuyentes_activos,
+        ], 200, JSON_PRETTY_PRINT);
     }
 
 }
--- website/View/Estadisticas/index.php
+++ website/View/Estadisticas/index.php
@@ -3,61 +3,119 @@
 </div>
 
 <div class="row">
-    <div class="col-md-3 bg-info" style="padding:2em">
-        <div class="row">
-            <div class="col-md-4">
-                <span class="fa fa-building" style="font-size:56px"></span>
-            </div>
-            <div class="col-md-8 text-center">
-                <span class="h1"><?=num($contribuyentes_sii)?></span><br/>
-                <span>Contribuyentes SII</span>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-3 bg-warning" style="padding:2em">
-        <div class="row">
-            <div class="col-md-4">
-                <span class="fa fa-users" style="font-size:56px"></span>
-            </div>
-            <div class="col-md-8 text-center">
-                <span class="h1"><?=num($usuarios_registrados)?></span><br/>
-                <span>Usuarios registrados</span>
+    <div class="col-md-3 col-sm-6">
+        <div class="panel panel-red">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-users fa-4x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div style="font-size:36px"><?=num($contribuyentes_sii)?></div>
+                        <div>Proveedores y/o clientes</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    <div class="col-md-3 bg-success" style="padding:2em">
-        <div class="row">
-            <div class="col-md-4">
-                <span class="fa fa-cloud" style="font-size:56px"></span>
-            </div>
-            <div class="col-md-8 text-center">
-                <span class="h1"><?=num($empresas_registradas)?></span><br/>
-                <span>Empresas registradas</span>
+    <div class="col-md-3 col-sm-6">
+        <div class="panel panel-green">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-rebel fa-4x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div style="font-size:36px"><?=num($usuarios_registrados)?></div>
+                        <div>Usuarios registrados</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    <div class="col-md-3 bg-danger" style="padding:2em">
-        <div class="row">
-            <div class="col-md-4">
-                <span class="fa fa-file" style="font-size:56px"></span>
+    <div class="col-md-3 col-sm-6">
+        <div class="panel panel-yellow">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-building fa-4x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div style="font-size:36px"><?=num($empresas_registradas)?></div>
+                        <div>Empresas registradas</div>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-8 text-center">
-                <span class="h1"><?=num($documentos_emitidos)?></span><br/>
-                <span>Documentos emitidos</span>
+        </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-xs-3">
+                        <i class="fa fa-file fa-4x"></i>
+                    </div>
+                    <div class="col-xs-9 text-right">
+                        <div style="font-size:36px"><?=num($documentos_emitidos)?></div>
+                        <div>Documentos emitidos</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
 
-<div style="margin-top:2em">
-    <img src="<?=$_base?>/estadisticas/grafico_usuarios_ingreso" alt="Usuarios mensuales" class="center img-responsive thumbnail" >
+<div class="row">
+    <!-- PANEL IZQUIERDA -->
+    <div class="col-md-9">
+        <!-- graficos ventas y compras -->
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <i class="fa fa-bar-chart-o fa-fw"></i> Usuarios mensuales que iniciaron sesión por última vez
+                </div>
+                <div class="panel-body">
+                    <div id="grafico-usuarios_mensuales"></div>
+                </div>
+            </div>
+        </div>
+        <!-- fin graficos ventas y compras -->
+    </div>
+    <!-- FIN PANEL IZQUIERDA -->
+    <!-- PANEL DERECHA -->
+    <div class="col-md-3">
+        <!-- empresas activas -->
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <i class="fa fa-building fa-fw"></i>
+                Empresas con movimientos
+            </div>
+            <div class="panel-body">
+                <div class="list-group">
+<?php foreach ($contribuyentes_activos as $c): ?>
+                    <div class="list-group-item"><?=$c['razon_social']?></div>
+<?php endforeach; ?>
+                </div>
+            </div>
+        </div>
+        <!-- empresas activas -->
+        <a class="btn btn-info btn-block" href="<?=$_base?>/estadisticas/<?=$certificacion?'produccion':'certificacion'?>" role="button">
+            Ver datos de <?=$certificacion?'producción':'certificación'?>
+        </a>
+    </div>
+    <!-- FIN PANEL DERECHA -->
 </div>
 
-<?php
-if (!empty($contribuyentes_activos)) {
-    echo '<h2>Contribuyentes con movimientos</h2>',"\n";
-    echo '<ul>',"\n";
-    foreach ($contribuyentes_activos as $razon_social)
-        echo '<li>',$razon_social,'</li>',"\n";
-    echo '</ul>',"\n";
-}
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+<script>
+Morris.Bar({
+    element: 'grafico-usuarios_mensuales',
+    data: <?=json_encode($usuarios_mensuales)?>,
+    xkey: 'mes',
+    ykeys: ['usuarios'],
+    labels: ['Usuarios'],
+    resize: true
+});
+</script>
