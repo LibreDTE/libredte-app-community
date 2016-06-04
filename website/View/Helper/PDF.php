4d24ef488992e56fba50ed96e51597f1fbf23b13
Esteban De La Fuente Rubio <esteban@delaf.cl>
2016-06-04 00:22:41 -0400

    Se agrega base para PDFs en la aplicación

---
+++ website/View/Helper/PDF.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * LibreDTE
+ * Copyright (C) SASCO SpA (https://sasco.cl)
+ *
+ * Este programa es software libre: usted puede redistribuirlo y/o
+ * modificarlo bajo los términos de la Licencia Pública General Affero de GNU
+ * publicada por la Fundación para el Software Libre, ya sea la versión
+ * 3 de la Licencia, o (a su elección) cualquier versión posterior de la
+ * misma.
+ *
+ * Este programa se distribuye con la esperanza de que sea útil, pero
+ * SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita
+ * MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO.
+ * Consulte los detalles de la Licencia Pública General Affero de GNU para
+ * obtener una información más detallada.
+ *
+ * Debería haber recibido una copia de la Licencia Pública General Affero de GNU
+ * junto a este programa.
+ * En caso contrario, consulte <http://www.gnu.org/licenses/agpl.html>.
+ */
+
+namespace website;
+
+/**
+ * Helper para la generación de PDFs personalizados para LibreDTE
+ * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+ * @version 2016-02-17
+ */
+class View_Helper_PDF extends \sowerphp\general\View_Helper_PDF
+{
+
+    public $Contribuyente; ///< Empresa que está generando el PDF
+    public $titulo; ///< Título del PDF
+
+    /**
+     * Método que sobreescribe la cabecera del PDF para tener una personalizada
+     * para los informes del contribuyente
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+     * @version 2016-02-17
+     */
+    public function Header()
+    {
+        // nombre de la empresa
+        $this->SetFont('helvetica', 'B', 10);
+        $this->Texto($this->Contribuyente->razon_social, 15, 14, 'C', '80');
+        $this->Ln();
+        $this->Texto($this->Contribuyente->giro, 15, $this->GetY(), 'C', '80');
+        $this->Ln();
+        // titulo del archivo
+        $this->SetFont('helvetica', 'B', 14);
+        $this->Texto($this->titulo, 15, $this->GetY()+2, 'C', '185');
+        // colocar enlace a la página web
+        $this->SetFont('helvetica', 'B', 10);
+        $link = 'http'.(isset($_SERVER['HTTPS'])?'s':null).'://'.$_SERVER['HTTP_HOST'];
+        $this->Texto($link, 15, 20+$this->margin_top, 'R', 185, $link);
+        $this->Ln();
+        // colocar línea
+        $this->Line($this->GetX(), $this->GetY()+2, 201, $this->GetY()+2);
+    }
+
+}
