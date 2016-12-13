7f9f6bf266ed2a1117f2d67c123e323834b6e368
Esteban De La Fuente Rubio <esteban@delaf.cl>
2016-12-13 04:41:41 -0300

    Se agrega ruta para servicios web del módulo de pagos

--- website/Config/routes.php
+++ website/Config/routes.php
@@ -30,6 +30,10 @@
     'module' => 'Dte.Admin',
     'action' => 'api',
 ]);
+\sowerphp\core\Routing_Router::connect('/api/pagos/:controller/*', [
+    'module' => 'Pagos',
+    'action' => 'api',
+]);
 
 // ruta para consultar DTE
 \sowerphp\core\Routing_Router::connect('/consultar', [
