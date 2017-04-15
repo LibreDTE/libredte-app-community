7dc925912c885a554e75fc903f9b28c4191190c7
Esteban De La Fuente Rubio <esteban@delaf.cl>
2017-04-14 21:57:44 -0300

    se acorta enlace registro

--- website/View/Layouts/LibreDTE.php
+++ website/View/Layouts/LibreDTE.php
@@ -90,7 +90,7 @@ foreach ($_nav_website as $link=>$name) {
                     <ul class="nav navbar-nav navbar-right">
 <?php if (!$_Auth->logged()) : ?>
 <?php if (\sowerphp\core\Configure::read('app.self_register')) : ?>
-                        <li><a href="<?=$_base?>/usuarios/registrar"><span class="text-primary"> ¡Regístrate gratis!</span></a></li>
+                        <li><a href="<?=$_base?>/usuarios/registrar"><span class="text-primary"> ¡Regístrate!</span></a></li>
 <?php endif; ?>
                         <li><a href="<?=$_base?>/usuarios/ingresar"><span class="fa fa-sign-in" aria-hidden="true"></span> Iniciar sesión</a></li>
 <?php else : ?>
