95dfe6c498e870c83392ea2275ff3d6a71956dc5
Esteban De La Fuente Rubio <esteban@delaf.cl>
2017-08-19 15:13:09 -0300

    fix alt en img

--- website/View/Layouts/LibreDTE.php
+++ website/View/Layouts/LibreDTE.php
@@ -50,7 +50,7 @@ Aplicación oficial en https://libredte.cl
         <script>
             !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init', '<?=\sowerphp\core\Configure::read('ads.facebook.pixel')?>');fbq('track', 'PageView');
         </script>
-        <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=<?=\sowerphp\core\Configure::read('ads.facebook.pixel')?>&amp;ev=PageView&amp;noscript=1"/></noscript>
+        <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=<?=\sowerphp\core\Configure::read('ads.facebook.pixel')?>&amp;ev=PageView&amp;noscript=1" alt="Facebook pixel" /></noscript>
 <?php endif; ?>
     </head>
     <body>
