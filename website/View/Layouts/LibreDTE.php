e29e0d6109f79a775df33d3acae525a535c5d91e
Esteban De La Fuente Rubio <esteban@delaf.cl>
2017-08-21 13:09:48 -0300

    Se actualiza descripción (aplica sólo a oficial)

--- website/View/Layouts/LibreDTE.php
+++ website/View/Layouts/LibreDTE.php
@@ -12,7 +12,7 @@ Aplicación oficial en https://libredte.cl
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="LibreDTE ¡facturación electrónica libre para Chile!">
+        <meta name="description" content="Facturación electrónica, contabilidad, punto de venta, tienda electrónica, RRHH, inventario ¡y más!">
         <meta name="keywords" content="factura electrónica, facturación electrónica, sii, dte, software libre, open source">
         <meta name="author" content="SASCO SpA">
         <title><?=$_header_title?></title>
