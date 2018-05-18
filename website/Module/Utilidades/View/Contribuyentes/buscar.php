ef493cb31a516ed7cfba72457aeddc1a400a428e
Esteban De La Fuente Rubio <esteban@delaf.cl>
2018-05-18 14:19:52 -0400

    Se agregan links para ver autorización DTE y situación tributaria

--- website/Module/Utilidades/View/Contribuyentes/buscar.php
+++ website/Module/Utilidades/View/Contribuyentes/buscar.php
@@ -8,6 +8,10 @@ echo $f->input([
     'check' => 'notempty rut',
 ]);
 if (isset($Contribuyente)) {
+    $links_sii = [
+        '<a href="https://libredte.cl/dte/sii/contribuyente_autorizado/'.$Contribuyente->getRUT().'" target="_blank">Autorización emisión DTE</a>',
+        '<a href="https://libredte.cl/dte/sii/contribuyente_situacion_tributaria/'.$Contribuyente->getRUT().'" target="_blank">Situación tributaria</a>',
+    ];
     echo $f->input(['type'=>'div', 'label'=>'Razón social', 'value'=>$Contribuyente->razon_social]);
     echo $f->input(['type'=>'div', 'label'=>'Giro', 'value'=>$Contribuyente->giro]);
     echo $f->input(['type'=>'div', 'label'=>'Actividad económica', 'value'=>$Contribuyente->getActividadEconomica()->actividad_economica.' ('.$Contribuyente->actividad_economica.')']);
@@ -17,5 +21,6 @@ if (isset($Contribuyente)) {
     echo $f->input(['type'=>'div', 'label'=>'Sitio web', 'value'=>'<a href="'.$Contribuyente->config_extra_web.'" target="_blank">'.$Contribuyente->config_extra_web.'</a>']);
     echo $f->input(['type'=>'div', 'label'=>'Intercambio', 'value'=>$Contribuyente->config_email_intercambio_user]);
     echo $f->input(['type'=>'div', 'label'=>'Autorizado', 'value'=>$Contribuyente->config_ambiente_produccion_numero?'Si':'No']);
+    echo $f->input(['type'=>'div', 'label'=>'Ver en SII', 'value'=>implode(' / ', $links_sii)]);
 }
 echo $f->end('Buscar contribuyente');
