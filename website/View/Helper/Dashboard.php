9c070e5ca7b8a0b0b357f67b746c1cb2e32f7b78
Esteban De La Fuente Rubio <esteban@delaf.cl>
2018-10-21 00:38:06 -0300

    Se crea helper para lso Dashboards

---
+++ website/View/Helper/Dashboard.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * LibreDTE
+ * Copyright (C) SASCO SpA (https://sasco.cl)
+ *
+ * Este programa es software libre: usted puede redistribuirlo y/o
+ * modificarlo bajo los términos de la Licencia Pública General Affero de GNU
+ * publicada por la Fundación para el Software Libre, ya sea la versión
+ * 3 de la Licencia, o (a su elección) cualquier versión posterior de la
+ * misma.
+ *
+ * Este programa se distribuye con la esperanza de que sea útil, pero
+ * SIN GARANTÍA ALGUNA; ni siquiera la garantía implícita
+ * MERCANTIL o de APTITUD PARA UN PROPÓSITO DETERMINADO.
+ * Consulte los detalles de la Licencia Pública General Affero de GNU para
+ * obtener una información más detallada.
+ *
+ * Debería haber recibido una copia de la Licencia Pública General Affero de GNU
+ * junto a este programa.
+ * En caso contrario, consulte <http://www.gnu.org/licenses/agpl.html>.
+ */
+
+namespace website;
+
+/**
+ * Helper para la generación de los dashboards
+ * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+ * @version 2018-10-16
+ */
+class View_Helper_Dashboard
+{
+
+    /**
+     * Método que genera las tarjetas para el dashboard
+     * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
+     * @version 2018-10-16
+     */
+    public static function cards(array $cards, $plantilla = 'default')
+    {
+        $html = file_get_contents(__DIR__.'/Dashboard/'.$plantilla.'_cards.html');
+        $vars = [];
+        $n_cards = count($cards);
+        foreach($cards[0] as $key => $val) {
+            for ($i=1; $i<=$n_cards; $i++) {
+                $vars[] = '{card_'.$i.'_'.$key.'}';
+            }
+        }
+        $i = 1;
+        $vals = [];
+        foreach ($cards as $card) {
+            foreach($card as $key => $val) {
+                if ($key == 'quantity') {
+                    $val = num($val);
+                }
+                $vals['{card_'.$i.'_'.$key.'}'] = $val;
+            }
+            $i++;
+        }
+        sort($vars);
+        ksort($vals);
+        return str_replace($vars, $vals, $html);
+    }
+
+}
