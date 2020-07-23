164bea110b3552320acf54f8ba91be998dec837b
Esteban De La Fuente Rubio <esteban@delaf.cl>
2020-07-22 21:41:03 -0400

    Se pone en pausa las consultas a la API si hay error 423 o 429

--- website/basics.php
+++ website/basics.php
@@ -24,13 +24,14 @@
 /**
  * Función para consumir Servicios Web de la API de LibreDTE en api.libredte.cl
  * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
- * @version 2020-01-26
+ * @version 2020-07-22
  */
 function libredte_api_consume($recurso, $datos = [])
 {
+    // configuración de la API para funcionalidades extras
     $config = \sowerphp\core\Configure::read('proveedores.api.libredte');
     if (!$config) {
-        throw new \Exception('Funcionalidades extras no disponibles en esta versión de LibreDTE. Desbloquea las funcionalidades, desde costo 0, en api.libredte.cl');
+        throw new \Exception('Funcionalidades extras no disponibles en esta versión de LibreDTE. Desbloquea las funcionalidades, desde costo 0, en api.libredte.cl', 402);
     }
     if (!is_array($config)) {
         $config = [
@@ -38,12 +39,59 @@ function libredte_api_consume($recurso, $datos = [])
             'token' => $config,
         ];
     }
+    // verificar si se pueden hacer consultas a la API o la cuenta se encuentra
+    // en pausa por haber alcanzado el número máximo de consultas
+    $message_429 = 'Las consultas a la API de LibreDTE en api.libredte.cl se encuentran en pausa ya que se alcanzó el límite de la cuota permitida. Se podrán volver a hacer consultas después del %s. Recuperará el acceso a las funcionalidades extras de LibreDTE una vez se restablezca la cuota de consultas.';
+    $Cache = new \sowerphp\core\Cache();
+    $retry_time = $Cache->get('libredte_api_retry_time');
+    if ($retry_time) {
+        if (date('U') >= $retry_time) {
+            $Cache->delete('libredte_api_retry_time');
+            $Cache->delete('libredte_api_retry_message');
+        } else {
+            $retry_message = $Cache->get('libredte_api_retry_message');
+            if ($retry_message) {
+                $error_message = $retry_message;
+                $error_code = 423;
+            } else {
+                $error_message = sprintf($message_429, date('d/m/Y H:i:s', $retry_time));
+                $error_code = 429;
+            }
+            throw new \Exception($error_message, $error_code);
+        }
+    }
+    // realizar consulta a la API
     $LibreDTE = new \sasco\LibreDTE\API\LibreDTE($config['token'], $config['url']);
     try {
         $LibreDTE->consume($recurso, $datos);
     } catch (\sasco\LibreDTE\API\Exception $e) {
-        // falla silenciosamente, ya que se retorna arreglo completo que incluye
-        // el código de estado y cualquier error que pudiese haber ocurrido
+        // si falló por error 423 o 429, se pone en pausa las consultas a la API
+        // hasta que se pueda volver a consultar (hasta que se reestablezca la cuota)
+        // en estricto rigor 423 es bloqueo de la cuenta, pero se procesa como si
+        // fuese un 429 porque en teoría no se debería llegar a un error 423
+        // sólo si se usa la misma cuenta para hacer consultas por fuera de libredte
+        // o si se usa en más de un servidor o si se comparte IP en planes que no lo
+        // permiten
+        if (in_array($e->getCode(), [423, 429])) {
+            $headers = $LibreDTE->toArray()['header'];
+            if (!empty($headers['Retry-After'])) {
+                $retry_time = date('U') + $headers['Retry-After'];
+                $Cache->set('libredte_api_retry_time', $retry_time, 172800);
+                if ($e->getCode() == 429) {
+                    $error_message = sprintf($message_429, date('d/m/Y H:i:s', $retry_time));
+                } else {
+                    $retry_message = $e->getMessage();
+                    $Cache->set('libredte_api_retry_message', $retry_message, 172800);
+                    $error_message = $retry_message;
+                }
+                throw new \Exception($error_message, $e->getCode());
+            }
+        }
+        // el resto falla silenciosamente, ya que se retorna arreglo completo que
+        // incluye el código de estado y cualquier error que pudiese haber ocurrido
+        // esto se hizo así para mantener compatibilidad con el método antiguo
+        // de consulta a la API
     }
+    // entregar resultado normalizado como arreglo (cabecera y cuerpo)
     return $LibreDTE->toArray();
 }
