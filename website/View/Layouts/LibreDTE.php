2b2598b78f39927d406d116245edd0af53ce8dd6
Esteban De La Fuente Rubio <esteban@delaf.cl>
2021-06-24 18:20:59 -0400

    Se usa cdn.sasco.cl para JS

--- website/View/Layouts/LibreDTE.php
+++ website/View/Layouts/LibreDTE.php
@@ -32,8 +32,8 @@ Layout: harbor (https://hackerthemes.com)
                 _request = "<?=$_request?>"
             ;
         </script>
-        <script src="<?=$_base?>/js/__.js"></script>
-        <script src="<?=$_base?>/js/form.js"></script>
+        <script src="https://cdn.sasco.cl/js/__.min.js"></script>
+        <script src="https://cdn.sasco.cl/js/form.min.js"></script>
         <script src="<?=$_base?>/js/datepicker/bootstrap-datepicker.js"></script>
         <script src="<?=$_base?>/js/datepicker/bootstrap-datepicker.es.js"></script>
         <link rel="stylesheet" href="<?=$_base?>/js/datepicker/datepicker3.css" />
