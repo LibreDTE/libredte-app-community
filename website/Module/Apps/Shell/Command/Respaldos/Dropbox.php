65542eb8721b61329827cf2d971c7ed30a0fdedf
Esteban De La Fuente Rubio <esteban@delaf.cl>
2021-09-26 10:36:38 -0300

    Se agrega respaldo de sólo boletas electrónicas. Se actualiza módulo Dte.

--- website/Module/Apps/Shell/Command/Respaldos/Dropbox.php
+++ website/Module/Apps/Shell/Command/Respaldos/Dropbox.php
@@ -27,23 +27,23 @@ namespace website\Apps;
  * Comando para respaldar los datos de los contribuyentes en la cuenta asociada
  * a la aplicación de Dropbox
  * @author Esteban De La Fuente Rubio, DeLaF (esteban[at]sasco.cl)
- * @version 2021-06-04
+ * @version 2021-09-26
  */
 class Shell_Command_Respaldos_Dropbox extends \Shell_App
 {
 
-    public function main($grupo = null, $compress = 'tgz')
+    public function main($grupo = null, $compress = 'tgz', $fecha = null)
     {
         ini_set('memory_limit', '2048M');
         $contribuyentes = $this->getContribuyentes($grupo);
         foreach ($contribuyentes as $rut) {
-            $this->crearRespaldo($rut, $compress);
+            $this->respaldar($rut, $compress, $fecha);
         }
         $this->showStats();
         return 0;
     }
 
-    private function crearRespaldo($rut, $compress)
+    private function respaldar($rut, $compress, $fecha)
     {
         $Contribuyente = new \website\Dte\Model_Contribuyente($rut);
         // cargar dropbox
@@ -65,31 +65,51 @@ class Shell_Command_Respaldos_Dropbox extends \Shell_App
         if ($this->verbose) {
             $this->out('Respaldando el contribuyente '.$Contribuyente->razon_social.' en el Dropbox de '.$DropboxApp->getConfig()->display_name);
         }
-        $dir = (new \website\Dte\Admin\Model_Respaldo())->generar($Contribuyente->rut);
+        list($dir_uncompressed, $filename) = $this->crearRespaldo($Contribuyente, $fecha);
+        if ($dir_uncompressed === false) {
+            if ($this->verbose >= 2) {
+                $this->out('  No hay datos que respaldar');
+            }
+            return;
+        }
         \sowerphp\general\Utility_File::compress(
-            $dir, ['format'=>$compress, 'delete'=>true, 'download'=>false]
+            $dir_uncompressed, ['format'=>$compress, 'delete'=>true, 'download'=>false]
         );
-        $output = $dir.'.'.$compress;
+        $dir_compressed = $dir_uncompressed.'.'.$compress;
         // enviar respaldo a Dropbox
         try {
-            $archivo = date('N').'_'.\sowerphp\general\Utility_Date::$dias[date('w')];
             $file = $Dropbox->upload(
-                $output,
-                '/'.$Contribuyente->razon_social.'/respaldos/'.$archivo.'.'.$compress,
+                $dir_compressed,
+                '/'.$Contribuyente->razon_social.'/respaldos/'.$filename.'.'.$compress,
                 ['mode' => ['.tag' => 'overwrite']]
             );
             if ($this->verbose>=2) {
               $this->out('  Se subió el archivo: '.$file->getName());
             }
-            unlink($output);
+            unlink($dir_compressed);
             return true;
         } catch (\Exception $e) {
             if ($this->verbose) {
                 $this->out('  No se pudo subir el archivo: '.str_replace("\n", ' => ', $e->getMessage()));
             }
-            unlink($output);
+            unlink($dir_compressed);
             return false;
         }
+    }
+
+    private function crearRespaldo($Contribuyente, $fecha)
+    {
+        if ($Contribuyente->getUsuario()->inGroup('dte_plus')) {
+            $dir_uncompressed = (new \website\Dte\Admin\Model_Respaldo())->generar($Contribuyente->rut);
+            $filename = date('N').'_'.\sowerphp\general\Utility_Date::$dias[date('w')];
+        } else if ($Contribuyente->getUsuario()->inGroup('dte_mipyme')) {
+            if (!$fecha) {
+                $fecha = \sowerphp\general\Utility_Date::getPrevious(date('Y-m-d'), 'D');
+            }
+            $dir_uncompressed = (new \website\Dte\Admin\Model_Respaldo())->boletas($Contribuyente, $fecha);
+            $filename = $fecha.'_boletas';
+        }
+        return [$dir_uncompressed, $filename];
     }
 
     private function getContribuyentes($grupo = null)
@@ -98,32 +118,20 @@ class Shell_Command_Respaldos_Dropbox extends \Shell_App
             return [$grupo];
         }
         $db = \sowerphp\core\Model_Datasource_Database::get();
-        if ($grupo) {
-            return $db->getCol('
-                SELECT c.rut
-                FROM
-                    contribuyente AS c
-                    JOIN contribuyente_config AS cc ON cc.contribuyente = c.rut
-                    JOIN usuario AS u ON c.usuario = u.id
-                    JOIN usuario_grupo AS ug ON ug.usuario = u.id
-                    JOIN grupo AS g ON ug.grupo = g.id
-                WHERE
-                    g.grupo = :grupo
-                    AND cc.configuracion = \'apps\'
-                    AND cc.variable = \'dropbox\'
-            ', [':grupo' => $grupo]);
-        } else {
-            return $db->getCol('
-                SELECT c.rut
-                FROM
-                    contribuyente AS c
-                    JOIN contribuyente_config AS cc ON cc.contribuyente = c.rut
-                WHERE
-                    c.usuario IS NOT NULL
-                    AND cc.configuracion = \'apps\'
-                    AND cc.variable = \'dropbox\'
-            ');
-        }
+        return $db->getCol('
+            SELECT c.rut
+            FROM
+                contribuyente AS c
+                JOIN contribuyente_config AS cc ON cc.contribuyente = c.rut
+                JOIN usuario AS u ON c.usuario = u.id
+                JOIN usuario_grupo AS ug ON ug.usuario = u.id
+                JOIN grupo AS g ON ug.grupo = g.id
+            WHERE
+                g.grupo = :grupo
+                AND cc.configuracion = \'apps\'
+                AND cc.variable = \'dropbox\'
+            ORDER BY c.razon_social
+        ', [':grupo' => $grupo]);
     }
 
 }
