e1050ba8b494f26587e757f985678b7157959573
Esteban De La Fuente Rubio <esteban@delaf.cl>
2021-11-08 12:36:08 -0300

    'loading' al generar resumen de compras y ventas

--- website/Module/Dte/View/DteCompras/resumen.php
+++ website/Module/Dte/View/DteCompras/resumen.php
@@ -9,7 +9,7 @@
 <div class="page-header"><h1>Resumen libro de compras</h1></div>
 <?php
 $f = new \sowerphp\general\View_Helper_Form();
-echo $f->begin(['onsubmit'=>'Form.check()']);
+echo $f->begin(['onsubmit'=>'Form.check() && Form.loading(\'Generando estadísticas con el resumen...\')']);
 echo $f->input([
     'name' => 'anio',
     'label' => 'Año',
