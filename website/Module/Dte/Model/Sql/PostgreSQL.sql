af37ea4426d476d5501e8b654201e1132bfa10ca
Esteban De La Fuente Rubio <esteban@delaf.cl>
2022-07-12 15:45:20 -0400

    Se borran los dte_recibido cuando se borra dte_intercambio

--- website/Module/Dte/Model/Sql/PostgreSQL.sql
+++ website/Module/Dte/Model/Sql/PostgreSQL.sql
@@ -449,7 +449,7 @@ CREATE TABLE dte_recibido (
 		ON UPDATE CASCADE ON DELETE RESTRICT,
 	CONSTRAINT dte_recibido_intercambio_fk FOREIGN KEY (receptor, intercambio, certificacion)
 		REFERENCES dte_intercambio (receptor, codigo, certificacion)
-		ON UPDATE CASCADE ON DELETE RESTRICT
+		ON UPDATE CASCADE ON DELETE CASCADE
 );
 CREATE INDEX dte_recibido_fecha_emisor_idx ON dte_recibido (fecha, emisor);
 CREATE INDEX dte_recibido_receptor_emisor_idx ON dte_recibido (receptor, emisor);
