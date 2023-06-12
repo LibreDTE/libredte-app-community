d6704cab06d45f55eb822b3815a5f80ad26975b2
Felipe Badilla <f.badillape@gmail.com>
2023-06-12 14:34:03 -0400

    Mejora estetica modulo utilidades

--- website/Module/Utilidades/View/index.php
+++ website/Module/Utilidades/View/index.php
@@ -1,6 +1,66 @@
 <div class="page-header"><h1>Utilidades</h1></div>
-<div class="row">
-    <div class="col-md-4">
+<div class="row row-cols-3">
+    <div class="col">
+        <div class="list-group mb-4 card">
+            <div class="list-group-item list-group-item-primary">Boletas</div>
+            <div>
+                <a href="utilidades/boletas/rcof" title="Generar el XML del reporte de consumo de folios (RCOF)" class="list-group-item">
+                    <span class="far fa-file-code"></span> Generar RCOF
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="list-group mb-4 card">
+            <div class="list-group-item list-group-item-primary">Cesión electrónica (factoring)</div>
+            <div>
+                <a href="utilidades/factoring/ceder" title="Generar el XML de un archivo electrónico de cesión de un DTE" class="list-group-item">
+                    <span class="far fa-file-code"></span> Ceder documento electrónico
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="list-group mb-4 card">
+            <div class="list-group-item list-group-item-primary">Guías de despacho</div>
+            <div>
+                <a href="utilidades/guias/libro" title="Generar XML libro de guías de despacho a partir de un archivo CSV con los datos" class="list-group-item">
+                    <span class="fa fa-book"></span> Generar XML libro de guías
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="list-group mb-4 card">
+            <div class="list-group-item list-group-item-primary">XML</div>
+            <div>
+                <a href="utilidades/xml/firmar" title="Generar la firma de un XML e incluira en el mismo archivo" class="list-group-item">
+                    <span class="fa fa-certificate"></span> Firmar XML
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="list-group mb-4 card">
+            <div class="list-group-item list-group-item-primary">Contribuyentes</div>
+            <div>
+                <a href="utilidades/contribuyentes/buscar" title="Buscador datos contribuyente" class="list-group-item">
+                    <span class="fa fa-search"></span> Buscar contribuyente
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="list-group mb-4 card">
+            <div class="list-group-item list-group-item-primary">Firma electrónica</div>
+            <div>
+                <a href="utilidades/firma_electronica/datos" title="Ver datos de la firma electrónica" class="list-group-item">
+                    <span class="fa fa-certificate"></span> Datos de la firma
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="col">
         <div class="list-group mb-4 card">
             <div class="list-group-item list-group-item-primary">Documentos</div>
             <div>
@@ -24,71 +84,8 @@
                 </a>
             </div>
         </div>
-        <div class="list-group mb-4 card">
-            <div class="list-group-item list-group-item-primary">Boletas</div>
-            <div>
-                <a href="utilidades/boletas/rcof" title="Generar el XML del reporte de consumo de folios (RCOF)" class="list-group-item">
-                    <span class="far fa-file-code"></span> Generar RCOF
-                </a>
-            </div>
-        </div>
-        <div class="list-group mb-4 card">
-            <div class="list-group-item list-group-item-primary">Cesión electrónica (factoring)</div>
-            <div>
-                <a href="utilidades/factoring/ceder" title="Generar el XML de un archivo electrónico de cesión de un DTE" class="list-group-item">
-                    <span class="far fa-file-code"></span> Ceder documento electrónico
-                </a>
-            </div>
-        </div>
     </div>
-    <div class="col-md-4">
-        <div class="list-group mb-4 card">
-            <div class="list-group-item list-group-item-primary">Libro compra y venta</div>
-            <div>
-                <a href="utilidades/iecv/xml" title="Generar XML libro de compras y ventas a partir de un archivo CSV con los datos" class="list-group-item">
-                    <span class="fa fa-book"></span> Generar XML IECV
-                </a>
-            </div>
-            <div>
-                <a href="utilidades/iecv/pdf" title="Generar PDF a partir de un archivo XML de libro de compras y ventas" class="list-group-item">
-                    <span class="far fa-file-pdf"></span> Generar PDF IECV
-                </a>
-            </div>
-        </div>
-        <div class="list-group mb-4 card">
-            <div class="list-group-item list-group-item-primary">Guías de despacho</div>
-            <div>
-                <a href="utilidades/guias/libro" title="Generar XML libro de guías de despacho a partir de un archivo CSV con los datos" class="list-group-item">
-                    <span class="fa fa-book"></span> Generar XML libro de guías
-                </a>
-            </div>
-        </div>
-        <div class="list-group mb-4 card">
-            <div class="list-group-item list-group-item-primary">XML</div>
-            <div>
-                <a href="utilidades/xml/firmar" title="Generar la firma de un XML e incluira en el mismo archivo" class="list-group-item">
-                    <span class="fa fa-certificate"></span> Firmar XML
-                </a>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-4">
-        <div class="list-group mb-4 card">
-            <div class="list-group-item list-group-item-primary">Contribuyentes</div>
-            <div>
-                <a href="utilidades/contribuyentes/buscar" title="Buscador datos contribuyente" class="list-group-item">
-                    <span class="fa fa-search"></span> Buscar contribuyente
-                </a>
-            </div>
-        </div>
-        <div class="list-group mb-4 card">
-            <div class="list-group-item list-group-item-primary">Firma electrónica</div>
-            <div>
-                <a href="utilidades/firma_electronica/datos" title="Ver datos de la firma electrónica" class="list-group-item">
-                    <span class="fa fa-certificate"></span> Datos de la firma
-                </a>
-            </div>
-        </div>
+    <div class="col">
         <div class="list-group mb-4 card">
             <div class="list-group-item list-group-item-primary">Formatos DTE soportados</div>
             <div>
@@ -104,6 +101,21 @@
             <div>
                 <a href="utilidades/formatos/YAML" title="Convertir de YAML JSON" class="list-group-item">
                     <span class="fas fa-file-alt"></span> Formato oficial SII en YAML
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="col">
+        <div class="list-group mb-4 card">
+            <div class="list-group-item list-group-item-primary">Libro compra y venta</div>
+            <div>
+                <a href="utilidades/iecv/xml" title="Generar XML libro de compras y ventas a partir de un archivo CSV con los datos" class="list-group-item">
+                    <span class="fa fa-book"></span> Generar XML IECV
+                </a>
+            </div>
+            <div>
+                <a href="utilidades/iecv/pdf" title="Generar PDF a partir de un archivo XML de libro de compras y ventas" class="list-group-item">
+                    <span class="far fa-file-pdf"></span> Generar PDF IECV
                 </a>
             </div>
         </div>
