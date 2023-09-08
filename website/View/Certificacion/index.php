42e31e87af5cf7b01c011062ee8befab8ef9ea19
Esteban De La Fuente Rubio <esteban@delaf.cl>
2023-09-07 22:27:35 -0300

    Se empieza a limpiar doc no disponible

--- website/View/Certificacion/index.php
+++ website/View/Certificacion/index.php
@@ -17,17 +17,8 @@
 </div>
 
 <div class="row mb-4">
-    <div>
-        <a class="btn btn-primary btn-lg col-12" href="https://soporte.sasco.cl/kb/faq.php?id=38" role="button">
-            <span class="fas fa-list"></span>
-            Manual paso a paso y video tutoriales
-        </a>
-    </div>
-</div>
-
-<div class="row mb-4">
     <div class="col-md-4 mb-4">
-        <a href="https://libredte.cl/ventas/dte-cert" title="Solicitar Servicio de Certificación">
+        <a href="https://www.libredte.cl/shop/category/libredte-certificacion-dte-1" title="Solicitar Servicio de Certificación">
             <img src="https://libredte.cl/img/precios/libredte-certificacion-dte.jpg" alt="Con nosotros puedes certificar tus documentos ante el SII para ocupar tu propio sistema" class="img-fluid img-thumbnail" />
         </a>
     </div>
@@ -40,7 +31,7 @@
                     <hr class="my-4">
                     <p>Si ya tienes un software de mercado para emitir documentos tributarios electrónicos, pero falta que tu empresa tenga la autorización del SII, con este servicio quedará habilitada. Desde 30.000 pesos según los documentos que requieras certificar.</p>
                     <p>
-                        <a class="btn btn-primary col-12 btn-lg" href="https://libredte.cl/ventas/dte-cert" role="button">
+                        <a class="btn btn-primary col-12 btn-lg" href="https://www.libredte.cl/shop/category/libredte-certificacion-dte-1" role="button">
                             Solicitar Certificación de DTE a SASCO SpA
                         </a>
                     </p>
